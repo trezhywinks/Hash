d1624d7a6278d800c5ff58e852af6e2d13baf780
Trezhy Phantom <dreqxyonion@gmail.com>
2026-02-16 12:47:51 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require('cors');
 
@@ -8,23 +9,23 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
 
 app.post("/api/login", (req, res) => {
     const { username, password } = req.body;
-    const usersData = JSON.parse(fs.readFileSync("users.json"));
+    const usersPath = path.join(__dirname, "users.json");
+    const usersData = JSON.parse(fs.readFileSync(usersPath));
     const user = usersData.data.find(u => u.name === username);
 
     if (!user) return res.status(400).json({ error: "Usuário não existe" });
-
 
     if (password !== user.password) {
         return res.status(400).json({ error: "Senha incorreta" });
     }
 
-    
-    const dashboard = fs.readFileSync("public/dashboard.html", "utf-8");
+    const dashboardPath = path.join(__dirname, "public/dashboard.html");
+    const dashboard = fs.readFileSync(dashboardPath, "utf-8");
     res.send(dashboard);
 });
 
