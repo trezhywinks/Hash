b2bce27339d66b2d195484b334e8b7cd8c77d15a
Trezhy Phantom <dreqxyonion@gmail.com>
2026-02-16 13:11:56 -0300

    Atualizar o server.js

--- server.js
+++ server.js
@@ -12,17 +12,17 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static("public"));
+app.use(express.static("public")); 
 
 function checkAuth(req, res, next) {
-    const loggedUser = req.cookies.username;
-    if (!loggedUser) {
-        return res.redirect("/index.html"); 
+    if (req.cookies.username) {
+        next();
+    } else {
+        res.redirect("/index.html");
     }
-    next();
 }
 
-
+// Login
 app.post("/api/login", (req, res) => {
     const { username, password } = req.body;
     const usersPath = path.join(__dirname, "users.json");
@@ -32,18 +32,18 @@ app.post("/api/login", (req, res) => {
     if (!user) return res.status(400).json({ error: "Usuário não existe" });
     if (password !== user.password) return res.status(400).json({ error: "Senha incorreta" });
 
-    // Login válido → seta cookie
     res.cookie("username", username, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 }); // 1 dia
-    res.redirect("/dashboard"); // redireciona para o dashboard
+    res.redirect("/dashboard"); 
+
 });
 
+// Rota protegida para dashboard
 app.get("/dashboard", checkAuth, (req, res) => {
-    const dashboardPath = path.join(__dirname, "public/dashboard.html");
-    const dashboard = fs.readFileSync(dashboardPath, "utf-8");
-    res.send(dashboard);
+    const dashboardPath = path.join(__dirname, "u/dashboard.html");
+    res.sendFile(dashboardPath);
 });
 
-
+// Logout
 app.get("/logout", (req, res) => {
     res.clearCookie("username");
     res.redirect("/index.html");
