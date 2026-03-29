d4f7a9bfc9652838780fa085e3d1d25d96f5f081
Trezhy Phantom <dreqxyonion@gmail.com>
2026-03-29 01:05:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -52,6 +52,11 @@ app.get("/u", checkAuth, (req, res) => {
     res.sendFile(dashboardPath);
 });
 
+app.get("/css", checkAuth, (req, res) => {
+    const dashboardPath = path.join(__dirname, "u/chat.css");
+    res.sendFile(dashboardPath);
+});
+
 // Logout
 app.get("/logout", (req, res) => {
     res.clearCookie("username");
