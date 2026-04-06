2ad39155bdb612bcc4a80fa4dfd0755e9e6932af
Trezhy Phantom <dreqxyonion@gmail.com>
2026-04-06 04:16:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 //app.use(cors());
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: { origin: "*" }
-});
+//const io = new Server(server, {
+//  cors: { origin: "*" }
+//});
 
 
 const wss = new WebSocket.Server({ server });
