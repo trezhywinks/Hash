7976ee676409a58e43f9976f43e4dc15e579fe05
Trezhy Phantom <dreqxyonion@gmail.com>
2026-04-07 16:04:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const WebSocket = require("ws");
 const PORT = process.env.PORT || 3000;
 const app = express();
 //app.use(cors());
+//app.use(express.static("public")); 
 const server = http.createServer(app);
 //const io = new Server(server, {
 //  cors: { origin: "*" }
@@ -58,6 +59,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static("public")); 
 
 app.use((req, res, next) => {
   const ua = (req.headers['user-agent'] || "").toLowerCase();
