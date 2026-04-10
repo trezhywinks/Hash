13fda8db243b105a2c4965cdc384dd9bf3809951
Trezhy Phantom <dreqxyonion@gmail.com>
2026-04-10 22:58:11 +0200

    Update user.js

--- user.js
+++ user.js
@@ -1,58 +1,53 @@
-const WebSocket = require("ws");
-const server = require("http").createServer();
-
-const wss = new WebSocket.Server({ server });
 const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
 
 const app = express();
 const server = http.createServer(app);
-
+const wss = new WebSocket.Server({ server });
 
 let online = 0;
 const salas = {};
 
 wss.on("connection", (ws, req) => {
   online++;
-  
   console.log("🔥 Entrou:", online);
-  
+
   ws.send(JSON.stringify({ online }));
-  
   broadcast();
+
   const url = new URL(req.url, "http://localhost");
-  
+
   const user = url.searchParams.get("user");
   const to = url.searchParams.get("to");
-  
+
   if (user && to) {
     const chatId = [user, to].sort().join("_");
-    
+
     if (!salas[chatId]) {
       salas[chatId] = [];
     }
-    
+
     if (salas[chatId].length >= 2) {
       ws.send(JSON.stringify({ erro: "Sala cheia (2 usuários)" }));
       ws.close();
       return;
     }
-    
+
     salas[chatId].push(ws);
-    
+
     ws.chatId = chatId;
     ws.user = user;
-    
+
     console.log(`💬 ${user} entrou na sala ${chatId}`);
   }
-  
+
   ws.on("message", (msg) => {
     if (!ws.chatId) return;
-    
+
     const sala = salas[ws.chatId];
     if (!sala) return;
-    
+
     sala.forEach(client => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(JSON.stringify({
@@ -62,16 +57,16 @@ wss.on("connection", (ws, req) => {
       }
     });
   });
-  
+
   ws.on("close", () => {
     online--;
     console.log("Saiu:", online);
-    
+
     broadcast();
-    
+
     if (ws.chatId && salas[ws.chatId]) {
       salas[ws.chatId] = salas[ws.chatId].filter(c => c !== ws);
-      
+
       if (salas[ws.chatId].length === 0) {
         delete salas[ws.chatId];
       }
@@ -81,7 +76,7 @@ wss.on("connection", (ws, req) => {
 
 function broadcast() {
   const data = JSON.stringify({ online });
-  
+
   wss.clients.forEach(client => {
     if (client.readyState === WebSocket.OPEN) {
       client.send(data);
@@ -91,7 +86,6 @@ function broadcast() {
 
 app.use(express.static("users"));
 
-//server.listen(3000, () => {
-  server.listen(process.env.PORT || 3000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log("port = 3000 || ok");
 });
